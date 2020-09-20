8c2798c4345605ca3bf606664648ff8cbf36a791
h4y2t <hd4y2t@aol.com>
2020-09-20 21:37:08 +0700

    update

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -17,7 +17,7 @@ class MainPage extends StatelessWidget {
               color: Colors.white,
             ),
             title: Text(
-              "Page",
+              "MainPage",
               style: TextStyle(color: Colors.white),
             ),
             // actions: [
@@ -45,6 +45,113 @@ class MainPage extends StatelessWidget {
                         colors: [Color(0xFFF3FAD7), Color(0xFF60E06B)],
                         begin: Alignment.topCenter,
                         end: Alignment.bottomCenter)),
+              ),
+              Center(
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: Card(
+                      elevation: 10,
+                      child: Stack(
+                        children: [
+                          Opacity(
+                            opacity: 0.7,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png"),
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.35,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4),
+                                    topRight: Radius.circular(4)),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://moderndiplomacy.eu/wp-content/uploads/2018/03/greenplanet.jpg"),
+                                    fit: BoxFit.cover)),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(
+                                20,
+                                50 + MediaQuery.of(context).size.height * 0.35,
+                                20,
+                                20),
+                            child: Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Beautiful Sun",
+                                    maxLines: 2,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.green, fontSize: 25),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Posted on :",
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 12),
+                                        ),
+                                        Text(
+                                          "September 20, 2020",
+                                          style: TextStyle(
+                                              color: Colors.green,
+                                              fontSize: 12),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Spacer(
+                                        flex: 10,
+                                      ),
+                                      Icon(
+                                        Icons.thumb_up,
+                                        color: Colors.grey,
+                                      ),
+                                      Spacer(
+                                        flex: 1,
+                                      ),
+                                      Text(
+                                        "99",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      Spacer(
+                                        flex: 5,
+                                      ),
+                                      Icon(Icons.comment, color: Colors.grey),
+                                      Spacer(
+                                        flex: 1,
+                                      ),
+                                      Text(
+                                        "198",
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
+                                      Spacer(
+                                        flex: 10,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.end,
